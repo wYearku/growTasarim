6c7c7eb76b32bb61bbc3672dd0ca08aee7a79818
selim-haspolat <selimhaspolat@gmail.com>
2023-12-30 13:58:00 +0300

    yogun kar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
     <div
       className={`${poppins.className} bg-[#292B37] min-h-[100vh] text-white flex flex-col`}
     >
-      <Snowfall snowflakeCount={100} speed={[0.5, 1, 1.5, 2]} style={{ position: 'fixed', top: 0, zIndex: '9999'}}/>
+      <Snowfall snowflakeCount={10000} speed={[0.5, 1, 1.5, 2]} style={{ position: 'fixed', top: 0, zIndex: '9999'}}/>
       <NextNProgress color="#FFBA08" />
       <Navbar />
       <Component {...pageProps} />
