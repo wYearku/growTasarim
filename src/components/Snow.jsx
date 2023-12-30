1c261dbe26750f4bfe5b5cd2cdd2580fc00e0cd2
selim-haspolat <selimhaspolat@gmail.com>
2023-12-30 13:51:13 +0300

    snow animation changed

--- src/components/Snow.jsx
+++
@@ -1,9 +0,0 @@
-const Snow = ({ id, style }) => {
-  return (
-    <p className="Snowflake" id={`item${id}`} style={style}>
-      *
-    </p>
-  );
-};
-
-export default Snow;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,32 +8,17 @@ import NextNProgress from "nextjs-progressbar";
 
 import "@/styles/globals.css";
 import "@/styles/swiper.css";
-import Snow from "@/components/Snow";
+
+import Snowfall from 'react-snowfall'
 
 const poppins = Poppins({ subsets: ["devanagari"], weight: "400" });
 
 export default function App({ Component, pageProps }) {
-  const snow = () => {
-    let animationDelay = "0s";
-    let fontSize = "100px";
-    let arr = Array.from(
-      "Snowflakes are awesome!!! They are like little pieces of magic!!! Love snowflakes!!! Snowflakes are awesome!!! They are like little pieces of magic!!! Love snowflakes!!! Snowflakes are awesome!!! They are like little pieces of magic!!! Love snowflakes!!!"
-    );
-    return arr.map((el, i) => {
-      animationDelay = `${(Math.random() * 16).toFixed(2)}s`;
-      fontSize = `${Math.floor(Math.random() * 10) + 10}px`;
-      let style = {
-        animationDelay,
-        fontSize,
-      };
-      return <Snow key={i} id={i} style={style} />;
-    });
-  };
   return (
     <div
       className={`${poppins.className} bg-[#292B37] min-h-[100vh] text-white flex flex-col`}
     >
-      <div className="App w-full fixed top-0 z-50">{snow()}</div>
+      <Snowfall snowflakeCount={200} style={{ position: 'fixed', top: 0, zIndex: '9999'}}/>
       <NextNProgress color="#FFBA08" />
       <Navbar />
       <Component {...pageProps} />
