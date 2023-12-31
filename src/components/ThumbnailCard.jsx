56e29ea8b167d87716f8652f23c0967d81fc6f74
selim-haspolat <selimhaspolat@gmail.com>
2023-12-31 18:11:28 +0300

    asfas

--- src/components/ThumbnailCard.jsx
+++ src/components/ThumbnailCard.jsx
@@ -13,16 +13,23 @@ const ThumbnailCard = ({
   populer,
 }) => {
   return (
-    <div className="flex flex-col relative group text-black rounded-xl overflow-hidden">
+    <div
+      className={`flex flex-col w-[300px] h-[410px] relative group text-black rounded-xl overflow-hidden ${
+        populer ? "rainbow" : ""
+      } p-1`}
+    >
       {populer ? (
-        <Chip text="popüler" className={"absolute z-10 top-1 sm:top-2 left-1 sm:left-3 "} />
+        <Chip
+          text="popüler"
+          className={"absolute z-10 top-1 sm:top-2 left-1 sm:left-3 "}
+        />
       ) : null}
       <Link href={`/products/${url}`}>
         <Image
           width={10000000}
           height={10000000}
           src={image}
-          className="!w-full !h-[400px] object-cover group-hover:scale-110 transition-all rounded-xl duration-300 ease-in-out"
+          className="!w-full !h-[100%] object-cover group-hover:scale-110 transition-all rounded-lg duration-300 ease-in-out"
         />
         <div className="px-4 bg-[#1E2029] w-full bg-opacity-80 py-2 absolute bottom-0 text-white translate-y-10 group-hover:translate-y-0 transition-all duration-300 ease-in-out  ">
           <p>{title}</p>
