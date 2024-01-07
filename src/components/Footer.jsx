dbcbe7f01847387fad6ae999b90ae0993fd3540a
wYearku <craftrise1445@gmail.com>
2024-01-07 18:43:40 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -141,7 +141,7 @@ const Footer = () => {
         <div className="bg-[#3F4257] py-3 px-2">
           <div className="lg:w-[80%] mx-auto text-md:grid-cols-2 center">
             <p className="text-center">
-              Copyright 2023 <span className="text-[#ffba08]">GrowTasarım</span>
+              Copyright 2023 <span className="text-[#ffba08]">GrowTasarım </span>
               | Sitemizdeki yazı resim ve içeriklerin her hakkı saklıdır.
             </p>
           </div>
