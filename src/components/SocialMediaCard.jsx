1a6a3d1fb268ba33c7b7b55c44de9becb71494ad
wYearku <craftrise1445@gmail.com>
2024-01-22 23:21:22 +0300

    s

--- src/components/SocialMediaCard.jsx
+++ src/components/SocialMediaCard.jsx
@@ -5,7 +5,7 @@ const SocialMediaCard = ({ title, image, href }) => {
   return (
     <a href={href}>
       <div className="flex flex-col w-full h-44 xl:h-64 text-black rounded-2xl overflow-hidden">
-        <Image width={1000000000} height={1000000000} src={image} className="h-44 xl:h-64 -objectcover" />
+        <Image width={1000000000} height={1000000000} src={image} className="h-44 xl:h-64 object-cover" />
       </div>
     </a>
   );
